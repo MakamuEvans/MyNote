79c14b878228e0e7c7e2a2c4c05aa42729f3e1e9
makamuevans <makamuevans@gmail.com>
2017-07-09 17:53:14 +0300

    initial commit

---
+++ app/src/main/java/com/example/elm/login/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.example.elm.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //Write your logic here
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
