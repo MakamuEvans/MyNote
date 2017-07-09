6374d349d08fa56edaeb6ee7da17e500b571427d
makamuevans <makamuevans@gmail.com>
2017-07-10 00:28:07 +0300

    card almost there

--- app/src/main/java/com/example/elm/login/Navigation.java
+++ app/src/main/java/com/example/elm/login/Navigation.java
@@ -9,6 +9,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,6 +19,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import layout.EventsFragment;
+import layout.HomeFragment;
+import layout.NotesFragment;
+import layout.SummaryFragment;
 
 public class Navigation extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -120,9 +128,34 @@ public class Navigation extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-    public void tabActivity(View view){
-        Intent intent = new Intent(Navigation.this, TabActivity.class);
-        startActivity(intent);
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static Navigation.PlaceholderFragment newInstance(int sectionNumber) {
+            Navigation.PlaceholderFragment fragment = new Navigation.PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_home2, container, false);
+            return rootView;
+        }
     }
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
@@ -134,24 +167,39 @@ public class Navigation extends AppCompatActivity
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return TabActivity.PlaceholderFragment.newInstance(position + 1);
+            switch (position){
+                case 0:
+                    return HomeFragment.newInstance("param1", "param2");
+                case 1:
+                    return NotesFragment.newInstance("pram", "param");
+                case 2:
+                    return EventsFragment.newInstance("pram", "param");
+                case 3:
+                    return SummaryFragment.newInstance("pram", "param");
+                default:
+                    return HomeFragment.newInstance("param1", "param2");
+
+            }
         }
 
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 4;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Home";
                 case 1:
-                    return "SECTION 2";
+                    return "Notes";
                 case 2:
-                    return "SECTION 3";
+                    return "Events";
+                case 3:
+                    return "Summary";
+
             }
             return null;
         }
