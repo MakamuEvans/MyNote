16506291eeac1628336f183188b16d9fbb6d5146
makamuevans <makamuevans@gmail.com>
2017-07-11 10:20:25 +0300

    fail safe point

---
+++ app/src/main/java/com/example/elm/login/api/BasicAuth.java
@@ -0,0 +1,18 @@
+package com.example.elm.login.api;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
+/**
+ * Created by elm on 7/11/17.
+ */
+
+public class BasicAuth implements Interceptor {
+    private string
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        return null;
+    }
+}
