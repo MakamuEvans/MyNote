1071b2771ccc49c80032091489fb7f3006a47518
makamuevans <makamuevans@gmail.com>
2017-08-21 00:36:15 +0300

    ui almost

--- app/src/main/java/com/example/elm/login/adapter/MilestoneAdapter.java
+++ app/src/main/java/com/example/elm/login/adapter/MilestoneAdapter.java
@@ -1,13 +1,19 @@
 package com.example.elm.login.adapter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.chauthai.swipereveallayout.SwipeRevealLayout;
+import com.chauthai.swipereveallayout.ViewBinderHelper;
 import com.example.elm.login.R;
 import com.example.elm.login.model.Milestones;
 
@@ -19,6 +25,7 @@ import java.util.List;
 
 public class MilestoneAdapter extends RecyclerView.Adapter<MilestoneAdapter.myViewHolder> {
     public List<Milestones> allTasks;
+    private final ViewBinderHelper viewBinderHelper = new ViewBinderHelper();
 
     public MilestoneAdapter(List<Milestones> allTasks) {
         this.allTasks = allTasks;
@@ -42,6 +49,7 @@ public class MilestoneAdapter extends RecyclerView.Adapter<MilestoneAdapter.myVi
             holder.mSuccess.setVisibility(View.GONE);
             holder.imageView.setImageResource(R.mipmap.ic_action_check_circle);
         }
+        viewBinderHelper.bind(holder.swipeRevealLayout, milestones.getId().toString());
     }
 
     @Override
@@ -51,14 +59,44 @@ public class MilestoneAdapter extends RecyclerView.Adapter<MilestoneAdapter.myVi
 
     public class myViewHolder extends RecyclerView.ViewHolder{
         TextView title;
-        ImageView imageView;
+        ImageView imageView,mDelete;
         Button mSuccess, mFail;
+        SwipeRevealLayout swipeRevealLayout;
         public myViewHolder(final View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.milestone_card_title);
             imageView = (ImageView) itemView.findViewById(R.id.milestone_status);
             mSuccess = (Button) itemView.findViewById(R.id.milestone_success);
             mFail = (Button) itemView.findViewById(R.id.milestone_fail);
+            swipeRevealLayout = (SwipeRevealLayout) itemView.findViewById(R.id.swiper_milestone);
+            mDelete = (ImageView) itemView.findViewById(R.id.milestone_delete);
+
+            mDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final int pos = getLayoutPosition();
+                    final Milestones milestones = allTasks.get(pos);
+
+                    new AlertDialog.Builder(v.getContext())
+                            .setTitle("Are you sure?")
+                            .setMessage("You are about to delete Task "+milestones.getDescription())
+                            .setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            })
+                            .setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    milestones.delete();
+                                    removeTask(pos);
+                                }
+                            })
+                            .create()
+                            .show();
+                }
+            });
 
             mSuccess.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -96,6 +134,12 @@ public class MilestoneAdapter extends RecyclerView.Adapter<MilestoneAdapter.myVi
     }
     public void update(Milestones milestones){
 
+    }
+
+    public void removeTask(int position){
+        allTasks.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, allTasks.size());
     }
 
     public void delete(int position){
