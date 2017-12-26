bb0c9b925607ce15477fd7618761b05d3eccc386
Morris Murega <morris.murega@firstchoicegl.com>
2017-12-26 21:30:16 +0300

    home resize

---
+++ app/src/main/java/com/example/elm/login/SplashScreen.java
@@ -0,0 +1,17 @@
+package com.example.elm.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, Navigation.class);
+        startActivity(intent);
+        finish();
+    }
+}
