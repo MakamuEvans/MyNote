aff5018494fa23ba761fc3c81cd8898d5075eab7
Makamu Evans <makamuevans@gmail.com>
2019-02-02 16:13:11 +0300

    tests done

--- app/src/main/java/com/elm/mycheck/login/Homev2.java
+++ app/src/main/java/com/elm/mycheck/login/Homev2.java
@@ -24,32 +24,7 @@ public class Homev2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SharedPreferences preferences = getSharedPreferences("myPref", 0);
-        //Boolean fk = getSharedPreferences("myPref", 0).getBoolean("loggedIn", false);
-        String theme = getSharedPreferences("myPref", 0).getString("theme", "Default");
-        Log.e("Theme", theme);
-        if (theme == "tomato")
-            setTheme(R.style.AppTheme_NoActionBar);
-        if (theme == "tangarine")
-            setTheme(R.style.AppTheme_NoActionBar_Tangarine);
-        if (theme.equalsIgnoreCase("banana"))
-            setTheme(R.style.AppTheme_NoActionBar_Banana);
-        if (theme.equalsIgnoreCase("basil"))
-            setTheme(R.style.AppTheme_NoActionBar_Basil);
-        if (theme.equalsIgnoreCase("sage"))
-            setTheme(R.style.AppTheme_NoActionBar_Sage);
-        if (theme.equalsIgnoreCase("peacock"))
-            setTheme(R.style.AppTheme_NoActionBar_Peacock);
-        if (theme.equalsIgnoreCase("blueberry"))
-            setTheme(R.style.AppTheme_NoActionBar_BlueBerry);
-        if (theme.equalsIgnoreCase("lavender"))
-            setTheme(R.style.AppTheme_NoActionBar_Lavender);
-        if (theme.equalsIgnoreCase("grape"))
-            setTheme(R.style.AppTheme_NoActionBar_Grape);
-        if (theme.equalsIgnoreCase("flamingo"))
-            setTheme(R.style.AppTheme_NoActionBar_Flamingo);
-        if (theme.equalsIgnoreCase("graphite"))
-            setTheme(R.style.AppTheme_NoActionBar_Graphite);
+        setTheme(R.style.AppTheme_NoActionBar_Peacock);
         setContentView(R.layout.activity_homev2);
 
         List<Fragment> fragmentList = new ArrayList<>();
